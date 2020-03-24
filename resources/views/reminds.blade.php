a4bd9afbc7c59c8cc876cb3963b5783febc2884a
econron <50755382+econron@users.noreply.github.com>
2020-03-24 15:10:02 +0900

    登録した通知一覧画面の体裁を整えた。

--- resources/views/reminds.blade.php
+++ resources/views/reminds.blade.php
@@ -1,12 +1,24 @@
 @extends('layouts.app')
 @section('content')
-    @foreach($reminds as $remind)
-        <div>{{ $remind->channel_name }}</div>
-        <div>{{ $remind->remind_content }}</div>
-        <div>{{ $remind->webhook_address }}</div>
-        <div>{{ $remind->deadline }}</div>
-        <div><a href="{{ route('edit',  ['id' => $remind->id ]) }}">編集する</a></div>
-        <div><a href="{{ route('delete', ['id' => $remind->id ]) }}">削除する</a></div>
-    @endforeach
-    <div><a href="{{ route('adds') }}">通知を追加する</a></div>
+    <table border="1">
+        <tr>
+            <th>チャンネル名</th>
+            <th>リマインド内容</th>
+            <th>Webhookアドレス</th>
+            <th>締め切り日</th>
+            <th></th>
+            <th></th>
+        </tr>
+        @foreach($reminds as $remind)
+            <tr>
+                <td><div>{{ $remind->channel_name }}</div></td>
+                <td><div>{{ $remind->remind_content }}</div></td>
+                <td><div>{{ $remind->webhook_address }}</div></td>
+                <td><div>{{ $remind->deadline }}</div></td>
+                <td><div><a href="{{ route('edit',  ['id' => $remind->id ]) }}">編集する</a></div></td>
+                <td><div><a href="{{ route('delete', ['id' => $remind->id ]) }}">削除する</a></div></td>
+            </tr>
+        @endforeach
+    </table>
+    <button><a href="{{ route('adds') }}">通知を追加する</a></button>
 @endsection
